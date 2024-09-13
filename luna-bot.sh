d9e88c1be0fa077fc80900be92de2fc524a9f2c2
realstealthninja <68815218+realstealthninja@users.noreply.github.com>
2024-09-13 17:37:23 +0530

    feat: add luna-bot sh

---
+++ luna-bot.sh
@@ -0,0 +1,23 @@
+# DISCORD BOTS ALPINE LXC
+#
+# Runs a discord bot using openrc on boot
+#!/sbin/openrc-run
+
+
+command="/usr/local/bin/luna-bot"
+pidfile="/run/$RC_SVCNAME/$RC_SVCNAME.pid"
+command_args="-p ${pidfile} --make-pidfile --remove-pidfile"
+command_background=True
+output_log="/var/log/$RC_SVCNAME/luna-bot.log"
+error_log="/var/log/$RC_SVCNAME/luna-bot.err"
+
+
+start_pre() {
+        checkpath --directory --mode 0700 --owner root:root "/run/$RC_SVCNAME/"
+}
+
+
+
+depend() {
+        need net
+}
